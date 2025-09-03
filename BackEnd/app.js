5f073bb102e90e32c23a1ca7425d3e7620c0944a
Vitória Tedeia <163847549+vitoriatedeia@users.noreply.github.com>
2025-09-03 15:32:54 -0300

    Fim da aula

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -37,6 +37,128 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
+// Middleware
+//FRONT:
+/* await axios.get ("http://localhost:3001/auth/profile", {
+     header: { Authorization: `Bearer ${token}`},
+})
+*/
+function autenticarToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Token não fornecido!" })
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      res.status(403).json({ error: "Tokeninválido!" })
+    }
+    req.user = user;
+    next();
+  })
+}
+
+// Rota: REGISTRO
+app.post("/auth/register", async (req, res) => {
+  try {
+    const { nome, email, senha } = req.body;
+
+    if (!nome || !email || !senha) {
+      return res.status(400), json({ error: "Preencha todos os campos!" });
+    }
+
+    const [rows] = await pool.query("SELECT id FROM users WHERE email = ?", [
+      email,
+    ]);
+    if (rows.length > 0) {
+      return res.status(400).json({ error: "Email já cadastrado" });
+    }
+
+    const senha_hash = await bcrypt.hash(senha, 10);
+
+    await pool.query("INSERT INTO users (nome, email, senha) VALUE (?, ?, ?)", [
+      nome,
+      email,
+      senha_hash,
+    ]);
+
+    res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro ao registrar usuário!" });
+  }
+});
+
+// Rota: LOGIN
+app.post("/auth/login", async (req, res) => {
+  try {
+    const { email, senha } = req.body;
+
+    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+    if (rows.length === 0) {
+      return res.status(400).json({ error: "Usuário não encontrado!" });
+    }
+    const usuario = rows[0];
+
+    const senhaValida = await bcrypt.compare(senha, usuario.senha);
+    if (!senhaValida) {
+      return res.status(401).json({ error: "Senha incorreta" });
+    }
+
+    const token = jwt.sign(
+      { id: usuario.id, email: usuario.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ message: "Login bem sucedido!", token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro ao fazer login!" });
+  }
+});
+
+// Rota: PERFIL
+app.get("/auth/profile", autenticarToken, async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT nome, email FROM users WHERE id =?", [req.user.id])
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Usuário não encontrado." })
+    }
+
+    res.json({ user: rows[0] })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro ao buscar dados do usuário" })
+  }
+});
+
+// Rota: ATUALIZAR
+app.put("/auth/update", autenticarToken, async (req, res) => {
+  try {
+    const { nome, email } = req.body;
+
+    if (!nome && !email) {
+      return res.status(400).json({ error: "Informe nome ou email para atualizar" })
+    }
+
+    await pool.query(
+      "UPDATE users SET nome= ?, email = ?  WHERE  id = ?", [nome, email, req.user.id]
+    )
+    res.json({ message: "Dados atualizados com sucesso!" })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro ao atualizar usuário!" })
+  }
+})
+
 async function conexaoBd() {
   try {
     const conn = await pool.getConnection();

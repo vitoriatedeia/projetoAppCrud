41e023aea833fe6cff41d50c69800b268f1cc10d
Vitória Tedeia <163847549+vitoriatedeia@users.noreply.github.com>
2025-09-03 12:17:27 -0300

    Primeiro commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -10,7 +10,46 @@ const cors = require("cors");
 // DOTENV -> Desta maneira não deixamos senhas, tokens e configs expostas no código!
 const dotenv = require("dotenv");
 
+// MYSQL2 -> Biblioteca para conectarmos o Node ao banco MYSQL
+// MYSQL2 -> Essa versão suporta Promises, permitindo usar async/await em consultas.
+const mysql = require("mysql2/promise");
+
+// BCRYPT -> Usado para criptografar as senhas.
+// BCRYPT -> Nunca devemos salvar as senhas pura no banco, por isso usamos o bcrypt.
+const bcrypt = require("bcrypt");
+
+// JSONWEBTOKEN -> Gerar tokens de validação.
+// JSONWEBTOKEN -> Quando o usuário loga, enviamos um JWT ao usuário, que será usado para acessar rotas potegidas.
+const jwt = require("jsonwebtoken");
+
+const PORT = 3001;
 const app = express();
 
 dotenv.config();
 app.use(cors());
+app.use(express.json());
+
+// Conexão com o banco MYSQL
+const pool = mysql.createPool({
+  host: process.env.DEB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+async function conexaoBd() {
+  try {
+    const conn = await pool.getConnection();
+    console.log("Conexão com o MYSQL bem sucedida!");
+    conn.release();
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+conexaoBd();
+
+// Inicialização do servidor
+app.listen(PORT, async () => {
+  console.log(`Servidor rodando na porta ${PORT}!`);
+});
